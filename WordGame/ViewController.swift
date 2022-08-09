e9b48594576175bb2c64a24229ebd97ad2aba31c
Haider Ali Kazal <haider.kazal@tigerit.com>
2022-08-10 01:40:49 +0600

    Initial Commit

---
+++ WordGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordGame
+//
+//  Created by Haider Ali Kazal on 10/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
